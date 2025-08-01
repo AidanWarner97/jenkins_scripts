4acaaabdb247ddf7ee2d835110cabcb5c46af7a1
AidanWarner97 <aidan@awarner.co.uk>
2025-08-01 11:29:55 +0200

    Add Google Devices to clean

--- 16/01-preclean.sh
+++ 16/01-preclean.sh
@@ -11,6 +11,12 @@ for device in $devices; do
     if [[ -d vendor/$device ]]; then rm -rf vendor/$device; fi
 done
 
+google_dt="device/google"
+pixels="oriole raven bluejay panther cheetah lynx felix tangorpro shiba husky akita tokay caiman komodo comet tegu"
+for pixel in $pixels; do
+    if [[ $google_dt/$pixel ]]; then rm -rf $google_dt/$pixel; fi
+done
+
 if [[ -d "hardware/xiaomi" ]]; then
     rm -rf hardware/xiaomi
 fi
