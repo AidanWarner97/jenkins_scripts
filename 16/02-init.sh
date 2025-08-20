4777456e8bccecb3235277346ff65ebc6ffce101
AidanWarner97 <aidan@awarner.co.uk>
2025-08-20 20:11:13 +0100

    Add mkdir for the signing keys Otherwise rsync fails

--- 16/02-init.sh
+++ 16/02-init.sh
@@ -3,4 +3,5 @@
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
+mkdir -p /mnt/evo/11-source/vendor/evolution-priv/
 rsync -av /mnt/evo/vendor/evolution-priv/keys/ /mnt/evo/11-source/vendor/evolution-priv/keys/
