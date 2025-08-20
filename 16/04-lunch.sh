69779b04f45fb0de412fe188cc3e96844aa8e2ff
AidanWarner97 <aidan@awarner.co.uk>
2025-08-20 06:46:51 +0100

    Init lunch.sh

---
+++ 16/04-lunch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+device=$1
+buildtype=$2
+
+target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
+
+if [ "$buildtype" == "userdebug" ]; then
+    export EVO_BUILD_TYPE=Official
+else
+    export EVO_BUILD_TYPE=Unofficial
+fi
+
+export CCACHE_MAXSIZE=300G
+
+export CFLAGS="$CFLAGS -isystem /usr/include/x86_64-linux-gnu"
+export CXXFLAGS="$CXXFLAGS -isystem /usr/include/x86_64-linux-gnu"
+export KERNEL_CFLAGS="$KERNEL_CFLAGS -isystem /usr/include/x86_64-linux-gnu"
+export KCFLAGS="$KCFLAGS -isystem /usr/include/x86_64-linux-gnu"
+
+source build/envsetup.sh &&
+lunch lineage_$device-$target-$buildtype &&
