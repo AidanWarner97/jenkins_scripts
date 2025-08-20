5c419250d6018b71a102e2ea129e55730b5768dc
AidanWarner97 <aidan@awarner.co.uk>
2025-08-20 19:56:40 +0100

    Add support for signing keys

--- 16/02-init.sh
+++ 16/02-init.sh
@@ -2,3 +2,5 @@
 
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+rsync -av /mnt/evo/vendor/evolution-priv/keys/ /mnt/evo/11-source/vendor/evolution-priv/keys/
