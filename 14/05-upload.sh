5321a8b3c9413ee2f48824af233e4fd844981112
AidanWarner97 <aidan@awarner.co.uk>
2025-09-13 14:55:52 +0100

    Refactor upload script to use dynamic folder naming for Vanilla builds

--- 14/05-upload.sh
+++ 14/05-upload.sh
@@ -7,9 +7,18 @@ filename=$(echo out/target/product/$device/EvolutionX-*.zip)
 version=$(echo $filename | cut -d "-" -f 2| cut -d "." -f 1)
 date=$(echo $filename | cut -d "-" -f 3 | cut -d "." -f 1)
 
+# Check if filename contains "Vanilla" and set folder accordingly
+if [[ "$filename" == *"Vanilla"* ]]; then
+    folder="${date}_Vanilla"
+    echo "Vanilla build detected - using folder: $folder"
+else
+    folder="$date"
+    echo "Standard build detected - using folder: $folder"
+fi
+
 # Upload main rom
 echo "Uploading main rom..."
-rclone copy out/target/product/$device/EvolutionX*.zip b2:evo-downloads/$device/$date/ -P
+rclone copy out/target/product/$device/EvolutionX*.zip b2:evo-downloads/$device/$folder/ -P
 echo " "
 
 # Identify and upload initial install images
@@ -21,6 +30,6 @@ initial_images=$(jq -r '.response[0].initial_installation_images[]' "$json")
 # Upload found images
 for image in $initial_images; do
     echo "Uploading $image..."
-    rclone copy out/target/product/$device/$image*.img b2:evo-downloads/$device/$date/$image/ -P
+    rclone copy out/target/product/$device/$image*.img b2:evo-downloads/$device/$folder/$image/ -P
     echo " "
 done
