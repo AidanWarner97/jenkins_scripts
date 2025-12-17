ec60749a4d06cc69282669843ef8d02b83ff031e
AidanWarner97 <aidan@awarner.co.uk>
2025-12-17 23:58:24 +0100

    Scripts: Update updateStatus.sh Logic
    * Commonize the updateStatus.sh as it does not matter which version it builds
    * Read Evolution X version from vendor/lineage/config/version.mk

--- updateStatus.sh
+++ updateStatus.sh
@@ -2,13 +2,21 @@
 
 # === Config ===
 CODENAME="$1"
-STATUS="$2"
-FILENAME="${3:-}"
-API_URL="https://dashboard.evolution-x.nl/_API/buildStatus.php?action=updateStatus"
+BUILDTYPE="$2"
+STATUS="$3"
+PATH_INPUT="$4"
+
+EvoVersion=$(
+    grep -E '^EVO_VERSION_BASE' "$PATH_INPUT/vendor/lineage/config/version.mk" \
+    | awk -F':= *' '{print $2}' | tr -d ' '
+)
+
+API_URL="https://mm-dashboard.evolution-x.org/_API/buildStatus.php?action=updateStatus"
 
 # === cURL Request ===
 curl -X POST "$API_URL" \
      -H "Content-Type: application/x-www-form-urlencoded" \
      --data-urlencode "c=$CODENAME" \
-     --data-urlencode "f=$FILENAME"
-     --data-urlencode "s=$STATUS"
+     --data-urlencode "bt=$BUILDTYPE" \
+     --data-urlencode "s=$STATUS" \
+     --data-urlencode "ev=$EvoVersion"
