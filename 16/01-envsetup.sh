15db4cafc609d9629a237cca591738f6489d78c9
AidanWarner97 <aidan@awarner.co.uk>
2026-06-26 09:37:28 +0000

    envsetup: Add memory limits for RSYNC

--- 16/01-envsetup.sh
+++ 16/01-envsetup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set resource for rsync
+export RSYNC_MAX_ALLOC=4G
+
 # Updating from source
 echo "Creating environment from source snapshot..."
 rsync -ah --delete /home/shared/11-source/ /home/shared/11-build/
