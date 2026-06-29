2409be3b7af63626d971b8590446dda6fddb032b
AidanWarner97 <aidan@awarner.co.uk>
2026-06-29 08:46:04 +0000

    scripts: Optimize local source size for vic

--- 15/02-init.sh
+++ 15/02-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init -u https://github.com/Evolution-X/manifest -b vic --depth=1 --git-lfs
+repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
 git clone git@github.com:Evolution-X/vendor_evolution-priv_keys.git /home/shared/10-source/vendor/evolution-priv/keys/
