472333bd5e50b01db55eca7a55b765a68c9b5d8e
AidanWarner97 <aidan@awarner.co.uk>
2026-06-29 08:53:28 +0000

    scripts: Fetch keys from Evolution-X org directly

--- 16/02-init.sh
+++ 16/02-init.sh
@@ -3,5 +3,4 @@
 repo init -u https://github.com/Evolution-X/manifest -b bka --depth=1 --git-lfs
 repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
-mkdir -p /home/shared/11-source/vendor/evolution-priv/
-rsync -av /home/shared/vendor/evolution-priv/keys/ /home/shared/11-source/vendor/evolution-priv/keys/
+git clone git@github.com:Evolution-X/vendor_evolution-priv_keys_16_and_up.git -b bka /home/shared/11-source/vendor/evolution-priv/keys/
--- 17/02-init.sh
+++ 17/02-init.sh
@@ -3,5 +3,4 @@
 repo init -u https://github.com/Evolution-X/manifest -b cnb --depth=1 --git-lfs
 repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
-mkdir -p /home/shared/12-source/vendor/evolution-priv/
-rsync -av /home/shared/vendor/evolution-priv/keys/ /home/shared/12-source/vendor/evolution-priv/keys/
+git clone git@github.com:Evolution-X/vendor_evolution-priv_keys_16_and_up.git -b cnb /home/shared/12-source/vendor/evolution-priv/keys/
