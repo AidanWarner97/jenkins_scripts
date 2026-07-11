f8d40871d917ca353a24b381586ff4a60370fca6
AidanWarner97 <aidan@awarner.co.uk>
2026-07-11 20:34:21 +0100

    scripts: Update environment setup to accept device parameter for JSON removal

--- 16/01-envsetup.sh
+++ 16/01-envsetup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+device="$1"
+
 # Set resource for rsync
 export RSYNC_MAX_ALLOC=4G
 
 # Updating from source
 echo "Creating environment from source snapshot..."
 rsync -ah --delete /home/shared/11-source/ /home/shared/11-build/
+
+# Remove build data json
+rm "$device.json"
